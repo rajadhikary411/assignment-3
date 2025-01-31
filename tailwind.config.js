68961c7789df8fd403c47b34aed55263ac11e055
Raj Adhikary <radhikary149@gmail.com>
2025-01-31 18:54:43 +0600

    navber is done both mobile and dekstop

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"], // HTML ও JS ফাইলের পাথ দিন
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
